a7bfd2f2eef77b3b38e819f5d037493eab88581b
Joseph Guenther <joseph@codingcool.ca>
2023-08-28 16:30:56 -0400

    Switch view model

---
+++ TestApp/MainViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using DeveloperCore.Pusher;
+
+namespace TestApp;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    private readonly Sender _s = new(@"http://localhost:7166/", "test");
+    private readonly Receiver _r = new(@"ws://localhost:7166/", "test");
+    private bool _bound;
+    private string _data = "";
+
+    public string Data
+    {
+        get => _data;
+        set => SetField(ref _data, value);
+    }
+
+    public string ConnectText => _r.Connected ? "Disconnect" : "Connect";
+    public string BindText => _bound ? "Unbind" : "Bind";
+    
+    public async Task Connect()
+    {
+        if (_r.Connected)
+        {
+            await _r.DisconnectAsync();
+            OnPropertyChanged(nameof(ConnectText));
+        }
+        else
+        {
+            await _r.ConnectAsync();
+            OnPropertyChanged(nameof(ConnectText));
+        }
+    }
+    
+    public void Bind()
+    {
+        Action<Notification> action = (data) => Data += $"{data.Data}{Environment.NewLine}";
+        if (_bound)
+        {
+            _r.Unbind("Nice", action);
+            _bound = false;
+            OnPropertyChanged(nameof(BindText));
+        }else
+        {
+            _r.Bind("Nice", action);            
+            _bound = true;
+            OnPropertyChanged(nameof(BindText));
+        }
+    }
+    
+    public async Task Send()
+    {
+        await _s.Send("Nice", new { name = "hello", age = 9 });
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
--- TestApp/MainWindow.axaml.cs
+++ TestApp/MainWindow.axaml.cs
@@ -7,50 +7,9 @@ namespace TestApp;
 
 public partial class MainWindow : Window
 {
-    readonly Sender s = new(@"http://localhost:7166/", "test");
-    readonly Receiver r = new(@"ws://localhost:7166/", "test");
 
     public MainWindow()
     {
         InitializeComponent();
-        btnSend.Click += BtnSend_Click;
-        btnConnect.Click += BtnConnect_Click;
-        btnBind.Click += BtnBind_Click;
-    }
-
-    bool _bound = false;
-    private void BtnBind_Click(object? sender, RoutedEventArgs e)
-    {
-        Action<Notification> action = (data) => txtData.Text += $"{data.Data}{Environment.NewLine}";
-        if (_bound)
-        {
-            r.Unbind("Nice", action);
-            _bound = false;
-            btnBind.Content = "Bind";
-        }else
-        {
-            r.Bind("Nice", action);            
-            _bound = true;
-            btnBind.Content = "Unbind";
-        }
-    }
-
-    private async void BtnConnect_Click(object? sender, RoutedEventArgs e)
-    {
-        if (r.Connected)
-        {
-            await r.DisconnectAsync();
-            btnConnect.Content = "Connect";
-        }
-        else
-        {
-            await r.ConnectAsync();
-            btnConnect.Content = "Disconnect";
-        }
-    }
-
-    private async void BtnSend_Click(object? sender, RoutedEventArgs e)
-    {
-        await s.Send("Nice", new { name = "hello", age = 9 });
     }
 }
