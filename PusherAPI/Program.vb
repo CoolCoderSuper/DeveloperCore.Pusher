69735915ee9bd54e39d14c39bc4a0fbdafcee28b
Joseph Guenther <joseph@codingcool.ca>
2025-05-12 16:06:26 -0400

    Upgrade to .net 10 and other deps

--- PusherAPI/Program.vb
+++ PusherAPI/Program.vb
@@ -1,5 +1,6 @@
 Imports Microsoft.AspNetCore.Builder
 Imports Microsoft.Extensions.DependencyInjection
+Imports Scalar.AspNetCore
 
 Public Module Program
     Public Sub Main(args As String())
@@ -7,16 +8,16 @@ Public Module Program
         NotificationService.Key = builder.Configuration.Item("Key")
         builder.Services.AddControllers()
         builder.Services.AddEndpointsApiExplorer()
-        builder.Services.AddSwaggerGen()
+        builder.Services.AddOpenApi()
         builder.Services.AddSingleton(Of NotificationService)
         Dim app As WebApplication = builder.Build()
-        app.UseSwagger()
-        app.UseSwaggerUI()
         app.UseAuthorization()
         app.MapControllers()
         app.UseWebSockets()
         app.MapGet("/", Function() "yo were in")
         app.Map("/ws", AddressOf NotificationService.SocketHandler)
+        app.MapOpenApi()
+        app.MapScalarApiReference()
 #If DEBUG Then
         app.Run("http://localhost:7166")
 #Else
